ceddaae5bc821b8f278ab26b87af624dfe03feef
bd-fe <bdfe.info@gmail.com>
2019-12-04 17:15:16 +0900

    add vue init

---
+++ precache-manifest.60cede10c417666cd91e861027975dc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e56d916c46c109d31d54",
+    "url": "/test/css/app.faf618be.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/test/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "69a85ed3d07d9022c52af8ab6169bccf",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "e56d916c46c109d31d54",
+    "url": "/test/js/app.8bb43477.js"
+  },
+  {
+    "revision": "65497dbeec28b37c1af0",
+    "url": "/test/js/chunk-vendors.bfe99981.js"
+  },
+  {
+    "revision": "55d188a140581c464b2106acad466fc6",
+    "url": "/test/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/test/robots.txt"
+  }
+]);
